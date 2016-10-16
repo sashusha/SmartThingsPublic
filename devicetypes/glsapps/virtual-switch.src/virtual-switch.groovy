1562c85ef67ac1a08a275fa24f8481613b02964d
Alexander Vladimirov <grandcheburator@gmail.com>
2016-10-15 23:58:23 -0700

    Added virtual switch to control TV backlight through its parent smartapp

---
+++ devicetypes/glsapps/virtual-switch.src/virtual-switch.groovy
@@ -0,0 +1,58 @@
+/**
+ *  Copyright 2016 Alexander Vladimirov
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "Virtual Switch", namespace: "glsapps", author: "Alexander Vladimirov") {
+		capability "Actuator"
+ 		capability "Switch"
+		capability "Sensor"
+	}
+
+	// simulator metadata
+	simulator {
+		status "on":  "command: 2003, payload: FF"
+		status "off": "command: 2003, payload: 00"
+
+		// reply messages
+		reply "2001FF,delay 100,2502": "command: 2503, payload: FF"
+		reply "200100,delay 100,2502": "command: 2503, payload: 00"
+	}
+
+	// tile definitions
+	tiles(scale: 2) {
+		multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
+			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
+				attributeState "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821"
+				attributeState "off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
+			}
+		}
+
+		main "switch"
+		details(["switch","refresh"])
+	}
+}
+
+def parse(String description) {
+	log.debug "parse: ${description}"
+}
+
+def on() {
+	sendEvent(name: "switch", value: "on")
+	parent.on(this)
+}
+
+def off() {
+	sendEvent(name: "switch", value: "off")
+	parent.off(this)
+}
+
